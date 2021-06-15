99fb7cdcf6652ffa7d071ce65c6b13ee4616ceef
Junnewbe <85324653+Junnewbe@users.noreply.github.com>
2021-06-15 15:14:30 +0900

    2021-06-15update-js

---
+++ 06_15_pyto/fn.js
@@ -0,0 +1,12 @@
+function start_fnc(){
+        alert("ok")
+    }
+function mover(x){
+        x.style.backgroundColor='pink'
+    }
+function mleev(y){
+        y.style.backgroundColor='sky blue'
+    }
+function click(z){
+        z.style.backgroundColor='red'
+    }
